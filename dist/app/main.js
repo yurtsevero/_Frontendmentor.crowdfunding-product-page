2f6bd680c58b65b1bdcbfebf9cc6bde73c019b63
o.yrtsvr@gmail.com <o.yrtsvr@gmail.com>
2021-03-04 21:21:37 +0100

    mobile design withot modals

--- dist/app/main.js
+++ dist/app/main.js
@@ -1,6 +1,13 @@
 const hamburgerMenu = document.getElementById("icon__hamburger");
 const navMenu = document.getElementById("nav__menu");
 
+window.addEventListener("click", (e) => {
+  if (e.target == document.getElementsByClassName("modal")[0]) {
+    navMenu.classList.remove("open");
+    hamburgerMenu.src = "images/icon-hamburger.svg";
+    console.log("he");
+  }
+});
 hamburgerMenu.addEventListener("click", (e) => {
   e.preventDefault();
   navMenu.classList.toggle("open");
