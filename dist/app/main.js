2e8b97fee84d05d7d7699a9626093e0f3ca253b3
o.yrtsvr@gmail.com <o.yrtsvr@gmail.com>
2021-03-06 00:21:47 +0100

    selection modal

--- dist/app/main.js
+++ dist/app/main.js
@@ -1,11 +1,30 @@
 const hamburgerMenu = document.getElementById("icon__hamburger");
 const navMenu = document.getElementById("nav__menu");
+const btnBackThisProject = document.getElementById("main__btn");
+const selectionModalContainer = document.getElementById(
+  "selectionModalContainer"
+);
+const selectionModalClose = document.getElementById("selectionModalClose");
+const radio = document.querySelectorAll('input[name="product"]');
+
+radio.forEach((element) => {
+  element.addEventListener("change", () => {
+    console.log("hi moda");
+    const checkedradio = document.querySelector(
+      'input[name="product"]:checked'
+    );
+
+    console.log("here is some " + checkedradio.nextElementSibling);
+  });
+});
 
 window.addEventListener("click", (e) => {
   if (e.target == document.getElementsByClassName("modal")[0]) {
     navMenu.classList.remove("open");
     hamburgerMenu.src = "images/icon-hamburger.svg";
-    console.log("he");
+  }
+  if (e.target == selectionModalContainer) {
+    selectionModalContainer.classList.toggle("open");
   }
 });
 hamburgerMenu.addEventListener("click", (e) => {
@@ -16,4 +35,11 @@ hamburgerMenu.addEventListener("click", (e) => {
   } else {
     hamburgerMenu.src = "images/icon-hamburger.svg";
   }
+});
+
+btnBackThisProject.addEventListener("click", () => {
+  selectionModalContainer.classList.toggle("open");
+});
+selectionModalClose.addEventListener("click", () => {
+  selectionModalContainer.classList.toggle("open");
 });
