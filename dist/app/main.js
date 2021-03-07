e7097f8f59a1b949962667e37743cfd5923fc990
o.yrtsvr@gmail.com <o.yrtsvr@gmail.com>
2021-03-07 22:03:38 +0100

    modal open close

--- dist/app/main.js
+++ dist/app/main.js
@@ -5,16 +5,41 @@ const selectionModalContainer = document.getElementById(
   "selectionModalContainer"
 );
 const selectionModalClose = document.getElementById("selectionModalClose");
-const radio = document.querySelectorAll('input[name="product"]');
 
+const radio = document.querySelectorAll('input[name="product"]');
 radio.forEach((element) => {
   element.addEventListener("change", () => {
-    console.log("hi moda");
-    const checkedradio = document.querySelector(
-      'input[name="product"]:checked'
+    //Reset entered product
+    const productNotChecked = document.querySelectorAll(
+      'input[name="product"]:not(:checked)'
     );
 
-    console.log("here is some " + checkedradio.nextElementSibling);
+    productNotChecked.forEach((notCheckedElement) => {
+      try {
+        const productNCEnter =
+          notCheckedElement.parentElement.nextElementSibling.nextElementSibling
+            .nextElementSibling;
+        productNCEnter.classList.remove("open");
+      } catch (error) {
+        return;
+      }
+      const productNCCard = notCheckedElement.parentElement.parentElement;
+      productNCCard.classList.remove("product__card--active");
+    });
+
+    const productChecked = document.querySelector(
+      'input[name="product"]:checked'
+    );
+    try {
+      const productEnter =
+        productChecked.parentElement.nextElementSibling.nextElementSibling
+          .nextElementSibling;
+      productCard = productChecked.parentElement.parentElement;
+      productCard.classList.add("product__card--active");
+      productEnter.classList.add("open");
+    } catch (error) {
+      return;
+    }
   });
 });
 
@@ -25,6 +50,9 @@ window.addEventListener("click", (e) => {
   }
   if (e.target == selectionModalContainer) {
     selectionModalContainer.classList.toggle("open");
+  }
+  if (e.target == successModal) {
+    successModal.classList.toggle("open");
   }
 });
 hamburgerMenu.addEventListener("click", (e) => {
@@ -42,4 +70,22 @@ btnBackThisProject.addEventListener("click", () => {
 });
 selectionModalClose.addEventListener("click", () => {
   selectionModalContainer.classList.toggle("open");
+});
+
+//Continue  button pressed to support
+
+const btnContinue = Array.from(
+  document.getElementsByClassName("product__continue")
+);
+const successModal = document.getElementsByClassName("success")[0];
+
+btnContinue.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    selectionModalContainer.classList.toggle("open");
+    successModal.classList.toggle("open");
+  });
+});
+const successBtn = document.getElementsByClassName("success__btn")[0];
+successBtn.addEventListener("click", () => {
+  successModal.classList.remove("open");
 });
